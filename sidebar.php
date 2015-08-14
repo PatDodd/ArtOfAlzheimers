f8545f44c11c03b9de6c791a025e44217f4f6535
erictweigle <erictweigle@gmail.com>
2015-08-14 16:13:06 -0700

    image linking

--- sidebar.php
+++ sidebar.php
@@ -2,17 +2,17 @@
   <div class="latest-postings-sidebar">
     <a href="#"><h3>Latest Postings</h3>
     <a href="#"><div class="sidebar-latest-post">
-      <img src="images/sidebar-thumbnail-1.png" alt="latest postings sidebar thumbnail">
+      <img src="<?php bloginfo('template_directory'); ?>/images/sidebar-thumbnail-1.png" alt="latest postings sidebar thumbnail">
       <h4>Lorem Ipsum</h4>
       <small>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet.</small>
     </div></a>
     <a href="#"><div class="sidebar-latest-post">
-      <img src="images/sidebar-thumbnail-2.png" alt="latest postings sidebar thumbnail">
+      <img src="<?php bloginfo('template_directory'); ?>/images/sidebar-thumbnail-2.png" alt="latest postings sidebar thumbnail">
       <h4>Lorem Ipsum</h4>
       <small>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet.</small>
     </div></a>
     <a href="#"><div class="sidebar-latest-post">
-      <img src="images/sidebar-thumbnail-3.png" alt="latest postings sidebar thumbnail">
+      <img src="<?php bloginfo('template_directory'); ?>/images/sidebar-thumbnail-3.png" alt="latest postings sidebar thumbnail">
       <h4>Lorem Ipsum</h4>
       <small>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet.</small>
     </div></a>
